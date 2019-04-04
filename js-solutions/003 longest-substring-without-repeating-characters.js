906a6ce9f763c7a09b8a68340f99bf1f93ea5365
xgqfrms <xgqfrms@outlook.com>
2019-04-04 23:04:39 +0800

    Create 003 longest-substring-without-repeating-characters.js

---
+++ js-solutions/003 longest-substring-without-repeating-characters.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * @author xgqfrms
+ * @description leetcode problems: longest-substring-without-repeating-characters & https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * @language JavaScript & ES6
+ *
+ * @param {string} str
+ * @return {number}
+ * @created 2019.04.04
+ *
+ */
+
+const lengthOfLongestSubstring = (str = ``) => {
+    let result = 0;
+    return result;
+};
