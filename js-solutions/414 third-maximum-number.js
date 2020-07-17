06b5697ca41fdb4af197e5d24d05fa33ee8e2afd
xgqfrms <xgqfrms@outlook.com>
2020-07-17 15:59:34 +0800

    @solutions

--- js-solutions/414 third-maximum-number.js
+++ js-solutions/414 third-maximum-number.js
@@ -14,7 +14,55 @@
  * @augments
  * @example
  * @link https://leetcode.com/problems/third-maximum-number/
+ * @solutions https://leetcode.com/submissions/detail/367783973/
  *
  */
 
 const log = console.log;
+
+
+var thirdMax = function(nums) {
+  log(`\nnums`, nums)
+  const arr = [...new Set(nums)].sort((a, b) => b - a);
+  const len = arr.length;
+  log(`arr`, arr)
+  let result;
+  if(len < 3) {
+    result = arr[0];
+  } else {
+    result = arr[2];
+  }
+  log(`\nresult =`, result)
+  return result;
+};
+
+
+
+const test = [3, 2, 1];
+// 1
+const test2 = [1, 2];
+// 2
+const test3 = [2, 2, 3, 1];
+// 1
+
+const test4 = [1, 1, 2];
+// 2
+
+thirdMax(test)
+thirdMax(test2)
+thirdMax(test3)
+
+thirdMax(test4)
+// 2
+// result = undefined
+
+
+// very slowly
+// var thirdMax = function(nums) {
+//   const arr = nums.sort((a, b) => b - a).filter((item, i) => arr.indexOf(item) === i);
+//   if(arr.length < 3) {
+//     return arr[0];
+//   } else {
+//     return arr[2];
+//   }
+// };
