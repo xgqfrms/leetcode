d57b1dab522f5d87cd91209f5e7bf7875cd63f62
xgqfrms <xgqfrms@outlook.com>
2020-07-17 15:16:59 +0800

    @difficult Easy Medium Hard

---
+++ js-solutions/414 third-maximum-number.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-07-17
+ * @modified
+ *
+ * @description 414. Third Maximum Number
+ * @difficult Easy
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/third-maximum-number/
+ *
+ */
+
+const log = console.log;
+
+// Generator
+const  = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+
+export default ;
+
+export {
+  ,
+};
