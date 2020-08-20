c84b8595ea1d5ba971df1d4bb78c9907529d9fdd
xgqfrms <xgqfrms@outlook.com>
2020-08-21 00:49:24 +0800

    字节跳动面试题

---
+++ js-solutions/200 number-of-islands.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-08-20
+ * @modified
+ *
+ * @description
+ * @difficulty Medium
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/number-of-islands/
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+// number-of-islands/
