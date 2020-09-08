47a6388dc5354cfbd6d706482b74b8e7186f76f6
xgqfrms <xgqfrms@outlook.com>
2020-09-08 14:36:40 +0800

    shell
    https://www.cnblogs.com/xgqfrms/p/13632297.html

---
+++ 000-xyz/data-structure-all-in-one/shell/file.sh
@@ -0,0 +1,14 @@
+echo "Line 1"
+echo "Line 2"
+echo "Line 3"
+echo "Line 4"
+echo "Line 5"
+echo "Line 6"
+echo "Line 7"
+echo "Line 8"
+echo "Line 9"
+echo "Line 10"
+# bash file.txt | sed -n '10 p' ❌
+# bash file.sh | sed -n '10 p' ✅
+# zsh file.txt | sed -n '10 p' ❌
+# zsh file.sh | sed -n '10 p' ✅
---
+++ 000-xyz/data-structure-all-in-one/shell/readme.md
@@ -0,0 +1,26 @@
+# shell
+
+https://www.cnblogs.com/xgqfrms/p/13632297.html
+
+## sed
+
+```sh
+# bash file.txt | sed -n '10 p' ❌
+$ bash file.sh | sed -n '10 p' ✅
+
+# zsh file.txt | sed -n '10 p' ❌
+$ zsh file.sh | sed -n '10 p' ✅
+
+```
+
+## awk
+
+```sh
+
+```
+
+## head / tail
+
+```sh
+
+```
