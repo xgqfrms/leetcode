64f4f6c1dd9710da63b90a4e90ba4d42f69042c5
xgqfrms <xgqfrms@outlook.com>
2020-09-14 23:27:43 +0800

    Create get-random-mins.js

---
+++ 000-xyz/data-structure-all-in-one/get-random-mins.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-08-0
+ * @modified
+ *
+ * @description
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+/*
+
+网易一面
+
+在一组长度为n 的无序的数组中，取最小的 m个数(m < n), 要求时间复杂度 O(m * n)
+
+*/
