4bf46d98d067af26f51d6e835d6e237522aaf615
xgqfrms <xgqfrms@outlook.com>
2020-11-20 01:46:15 +0800

    Create 129 sum-root-to-leaf-numbers.js
    129. 求根到叶子节点数字之和

---
+++ js-solutions/129 sum-root-to-leaf-numbers.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-11-18
+ * @modified
+ *
+ * @description 129. sum root to leaf numbers
+ * @description 129. 求根到叶子节点数字之和
+ * @difficulty Medium
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/sum-root-to-leaf-numbers/
+ * @link https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+const log = console.log;
+
+
+
+
+
