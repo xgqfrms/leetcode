279ca70642b7e3f1ce11cb42e79380c94f7adb29
xgqfrms <xgqfrms@outlook.com>
2020-11-25 22:20:17 +0800

    Create 146 lru-cache.js

---
+++ js-solutions/146 lru-cache.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-11-26
+ * @modified
+ *
+ * @description 146. LRU Cache
+ * @description 146. LRU Cache
+ * @difficulty Medium
+ * @complexity O(n)
+ * @time O(n)
+ * @augments
+ * @example
+ * @link https://leetcode.com/problems/lru-cache/
+ * @link https://leetcode-cn.com/problems/lru-cache/
+ * @link https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU 最近最少使用（LRU）缓存算法
+ * @solutions
+ *
+ * @best_solutions
+ *
+ */
+
+const log = console.log;
+
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+
+
+/*
+
+// error
+const test1 = isValid(`((}}`);
+// ok
+const test2 = isValid(`()[]{}`);
+
+log(`❌test =`, test1 ? `✅` : `❌`);
+log(`✅test ok =`, test2 ? `✅` : `❌`);
+
+*/
